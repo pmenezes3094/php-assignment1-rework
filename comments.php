c2c3548d1b62436149bc9e1df7a7d2adf57157f2
pmenezes3094 <pmene.nic@gmail.com>
2023-09-11 23:34:30 -0700

    Add and update comments
    Updates the php file with a new comment line and a different comment format as required in 'adding comments to your code' section

--- comments.php
+++ comments.php
@@ -2,6 +2,8 @@
   /* 
   This page displays the member's name
   and details of a current offer
+
+  This is my second line of comment
   */
 ?>
 <!DOCTYPE html>
@@ -12,7 +14,7 @@
   </head>
   <body>
     <h1>The Candy Store</h1>
-    <h2><?php echo 'Welcome Ivy'; // Show name ?></h2>
+    <h2><?php echo 'Welcome Ivy'; # Show name ?></h2>
     <?php echo '<p class="offer">Offer: 20% off</p>' ?>
   </body>
 </html>
